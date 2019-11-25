b59baf138317cf3274a284bf423f6536cca5f091
Nestor Amichetti <nestor--@live.com>
2019-11-25 01:51:19 -0300

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina ={};
   cargada:boolean = false;
 
+  equipo:any[]= [];
+
   constructor( private http: HttpClient) { 
-
-    // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp:InfoPagina)=>{
-
-        this.cargada = true;
-        this.info = resp;
-
-
-      })
+    this.cargarInfo();
+    this.cargarEquipo();
   }
+
+  private cargarInfo() {
+    
+        // Leer el archivo JSON
+        this.http.get('assets/data/data-pagina.json')
+        .subscribe( (resp:InfoPagina)=>{
+  
+          this.cargada = true;
+          this.info = resp;
+  
+        });
+
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-1a957.firebaseio.com/equipo.json')
+        .subscribe( (resp:any[])=>{
+          this.equipo = resp;
+        });
+
+    
+  }
+
 }
 
